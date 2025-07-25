54f5ccf0d426ed74b2bf415619ed094974004bbe
yash56 <yash@connectex.io>
2025-07-25 17:40:55 -0600

    gujrati updated

--- data/gujrati.ts
+++ data/gujrati.ts
@@ -13,42 +13,39 @@
 // import Condition from "../assets/images/nepali/Baan.svg";ition.svg";
 // import Medicine from "../assets/images/nepali/Baan.svg";cine.svg";
 
-import Lotus from "../assets/images/nepali/Baan.svg";
-import Telescope from "../assets/images/nepali/Baan.svg";
-import Knot from "../assets/images/nepali/Baan.svg";
-import Grass from "../assets/images/nepali/Baan.svg";
-import Sanskrit from "../assets/images/nepali/Baan.svg";
-import Picture from "../assets/images/nepali/Baan.svg";
+import Lotus from "../assets/images/Lotus.svg";
+import Telescope from "../assets/images/Telescope.svg";
+import Knot from "../assets/images/Knot.svg";
+import Horse from "../assets/images/Horse.svg";
+import What from "../assets/images/What.svg"
+import Picture from "../assets/images/Picture.svg";
 import Umbrella from "../assets/images/nepali/Chata.svg";
-import Water from "../assets/images/nepali/Baan.svg";
-import Noise from "../assets/images/nepali/Baan.svg";
-import Knowledge from "../assets/images/nepali/Gyan.svg";
+import Water from "../assets/images/water.svg";
+import Coconut from "../assets/images/Coconut.svg";
+import Waterfall from "../assets/images/nepali/Jharna.svg";
 import Tomato from "../assets/images/Tomato.svg";
 import Cold from "../assets/images/nepali/Baan.svg";
-import Left from "../assets/images/nepali/Baan.svg";
-import Bat from "../assets/images/nepali/Baan.svg";
-import Shramann from "../assets/images/nepali/Baan.svg";
-import Sword from "../assets/images/nepali/Baan.svg";
-import Plate from "../assets/images/nepali/Baan.svg";
-import Milk from "../assets/images/nepali/Baan.svg";
-import Wealth from "../assets/images/nepali/Baan.svg";
-import Greeting from "../assets/images/nepali/Baan.svg";
+import Bucket from "../assets/images/Bucket.svg";
+import Devotee from "../assets/images/Devotee.svg";
+import Sword from "../assets/images/kannada/Khadga.svg";
+import Plate from "../assets/images/Plate.svg";
+import Milk from "../assets/images/Milk.svg";
+import Wealth from "../assets/images/Money.svg";
 import Bird from "../assets/images/nepali/Cara.svg";
 import Flower from "../assets/images/nepali/Fhul.svg";
 import Child from "../assets/images/nepali/Bachha.svg";
-import Roam from "../assets/images/nepali/Baan.svg";
+import Drum from "../assets/images/kannada/Dakke.svg";
 import House from "../assets/images/nepali/Ghara.svg";
-import Journey from "../assets/images/nepali/Baan.svg";
-import Interest from "../assets/images/nepali/Baan.svg";
+import Journey from "../assets/images/Journey.svg";
+import Bear from "../assets/images/nepali/Bhalu.svg";
 import Garlic from "../assets/images/nepali/Lasun.svg";
-import Occupation from "../assets/images/nepali/Baan.svg"
+import Occupation from "../assets/images/Occupation.svg"
 import Shame from "../assets/images/nepali/Baan.svg";
 import Hexagon from "../assets/images/nepali/Satkon.svg";
-import Sun from "../assets/images/nepali/Baan.svg";
-import Turmeric from "../assets/images/nepali/Baan.svg";
-import Maudik from "../assets/images/nepali/Baan.svg";
-import Damage from "../assets/images/nepali/Baan.svg";
-import Wise from "../assets/images/nepali/Baan.svg";
+import Sun from "../assets/images/Sun.svg";
+import Damage from "../assets/images/Damage.svg";
+import Elephant from "../assets/images/kannada/Aane.svg";
+import Wise from "../assets/images/Wise.svg";
 
 import { FlashCardType } from "../containers/flashCardTypes";
 
@@ -69,37 +66,37 @@ export const consonantCards: FlashCardType[] = [
     { letter: "ક", word: "કમલ", pronunciation: "Kamal", image: Lotus, translation: "Lotus flower" },
     { letter: "ખ", word: "ખગોળ", pronunciation: "Khagol", image: Telescope, translation: "Telescope" },
     { letter: "ગ", word: "ગાંઠ", pronunciation: "Gaanth", image: Knot, translation: "Knot" },
-    { letter: "ઘ", word: "ઘાસ", pronunciation: "Ghaas", image: Grass, translation: "Grass" },
-    { letter: "ઙ", word: "ઙણ", pronunciation: "Ṅa", image: Sanskrit, translation: "Sanskrit-derived" },
+    { letter: "ઘ", word: "ઘોડો", pronunciation: "Horse", image: Horse, translation: "Horse" },
+    { letter: "ઙ", word: "ઙણ", pronunciation: "Ṅa", image: What, translation: "" },
     { letter: "ચ", word: "ચિત્ર", pronunciation: "Chitra", image: Picture, translation: "Picture" },
     { letter: "છ", word: "છત્રી", pronunciation: "Chhatrī", image: Umbrella, translation: "Umbrella" },
     { letter: "જ", word: "જલ", pronunciation: "Jala", image: Water, translation: "Water" },
-    { letter: "ઝ", word: "ઝૂરો", pronunciation: "Jhūrō", image: Noise, translation: "Noise" },
-    { letter: "ઞ", word: "ઞાણ", pronunciation: "Gyaan", image: Knowledge, translation: "Knowledge" },
+    { letter: "ઝ", word: "ઝરણા", pronunciation: "jhar-naa", image: Waterfall, translation: "Waterfall" },
+    { letter: "ઞ", word: "", pronunciation: "", image: What, translation: "" },
     { letter: "ટ", word: "ટમેટા", pronunciation: "Tameta", image: Tomato, translation: "Tomato" },
     { letter: "ઠ", word: "ઠંડી", pronunciation: "Thandi", image: Cold, translation: "Cold" },
-    { letter: "ડ", word: "ડાબો", pronunciation: "Dabo", image: Left, translation: "Left" },
-    { letter: "ઢ", word: "ઢોંકો", pronunciation: "Dhoko", image: Bat, translation: "Bat" },
-    { letter: "ણ", word: "ણવ", pronunciation: "Shramann", image: Shramann, translation: "Specific Name" },
+    { letter: "ડ", word: "ડોલ", pronunciation: "Dol", image: Bucket, translation: "Bucket" },
+    { letter: "ઢ", word: "ઢોંકો", pronunciation: "Dhol", image: Drum, translation: "Drum" },
+    { letter: "ણ", word: "ણવ", pronunciation: "Shramann", image: What, translation: "" },
     { letter: "ત", word: "તલવાર", pronunciation: "Talvar", image: Sword, translation: "Sword" },
     { letter: "થ", word: "થાળી", pronunciation: "Thali", image: Plate, translation: "Plate" },
     { letter: "દ", word: "દૂધ", pronunciation: "Dudh", image: Milk, translation: "Milk" },
     { letter: "ધ", word: "ધન", pronunciation: "Dhan", image: Wealth, translation: "Wealth" },
-    { letter: "ન", word: "નમસ્તે", pronunciation: "Namastē", image: Greeting, translation: "Greeting" },
+    { letter: "ન", word: "નારીયળ", pronunciation: "Naa-ree-yal", image: Coconut, translation: "Coconut" },
     { letter: "પ", word: "પંખી", pronunciation: "Pankhi", image: Bird, translation: "Bird" },
     { letter: "ફ", word: "ફૂલ", pronunciation: "Phul", image: Flower, translation: "Flower" },
-    { letter: "બ", word: "બચ્ચો", pronunciation: "Baccho", image: Child, translation: "Child" },
-    { letter: "ભ", word: "ભટકવું", pronunciation: "Bhatakvu", image: Roam, translation: "Roam" },
+    { letter: "બ", word: "બાળક", pronunciation: "Baa-dak", image: Child, translation: "Child" },
+    { letter: "ભ", word: "ભક્ત", pronunciation: "Bhakta", image: Devotee, translation: "Devotee" },
     { letter: "મ", word: "મકાન", pronunciation: "Makan", image: House, translation: "House" },
     { letter: "ય", word: "યાત્રા", pronunciation: "Yatra", image: Journey, translation: "Journey" },
-    { letter: "ર", word: "રુચિ", pronunciation: "Ruchi", image: Interest, translation: "Interest" },
+    { letter: "ર", word: "રીંછ", pronunciation: "Reenchh", image: Bear, translation: "Bear" },
     { letter: "લ", word: "લસણ", pronunciation: "Lasan", image: Garlic, translation: "Garlic" },
     { letter: "વ", word: "વૃત્તિ", pronunciation: "Vruti", image: Occupation, translation: "Occupation" },
     { letter: "શ", word: "શરમ", pronunciation: "Sharam", image: Shame, translation: "Shame" },
     { letter: "ષ", word: "ષટક", pronunciation: "Shatak", image: Hexagon, translation: "Hexagon" },
     { letter: "સ", word: "સૂર્ય", pronunciation: "Surya", image: Sun, translation: "Sun" },
-    { letter: "હ", word: "હળદર", pronunciation: "Haldar", image: Turmeric, translation: "Turmeric" },
-    { letter: "ળ", word: "ળણ", pronunciation: "Maudik", image: Maudik, translation: "Specific Name" },
+    { letter: "હ", word: "હાથી", pronunciation: "Haathi", image: Elephant, translation: "Elephant" },
+    { letter: "ળ", word: "ળણ", pronunciation: "", image: What, translation: "" },
     { letter: "ક્ષ", word: "ક્ષતિ", pronunciation: "Kshati", image: Damage, translation: "Damage" },
     { letter: "જ્ઞ", word: "જ્ઞાની", pronunciation: "Gyaani", image: Wise, translation: "Wise person" },
 ];
