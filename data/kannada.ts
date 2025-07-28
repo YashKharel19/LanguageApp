3a5026f70f365b71c1acd1b298fee476c6ac9c22
yash56 <yash@connectex.io>
2025-07-28 14:57:01 -0600

    Kannada updated

--- data/kannada.ts
+++ data/kannada.ts
@@ -1,70 +1,89 @@
 // assets/data/consonants.ts
-
-// Import all local SVG files (based on English translation in PascalCase)
-import Akka from "../assets/images/kannada/Akka.svg";
+import Arasa from "../assets/images/kannada/Arasa.svg";
 import Elephant from "../assets/images/kannada/Aane.svg";
 import Mouse from "../assets/images/kannada/Mouse.svg";
-import Swim from "../assets/images/kannada/Swim.svg";
-import Laddu from "../assets/images/kannada/Laddu.svg";
-import Meal from "../assets/images/kannada/Meal.svg";
-import Leaf from "../assets/images/kannada/Leaf.svg";
-import What from "../assets/images/kannada/What.svg";
-import Five from "../assets/images/kannada/Five.svg";
-import Alone from "../assets/images/kannada/Alone.svg";
-import Read from "../assets/images/kannada/Read.svg";
-import Medicine from "../assets/images/kannada/Medicine.svg";
-import Monkey from "../assets/images/Monkey.svg";
-import Sword from "../assets/images/kannada/Sword.svg";
-import Wheat from "../assets/images/kannada/Wheat.svg";
-import Bell from "../assets/images/kannada/Bell.svg";
-import SmallPiece from "../assets/images/kannada/SmallPiece.svg";
-import Water from "../assets/images/kannada/Water.svg";
-import Hat from "../assets/images/kannada/Hat.svg";
+import Ungura from "../assets/images/kannada/Ungura.svg";
+import Meal from "../assets/images/kannada/Oota.svg";
+import What from "../assets/images/What.svg";
+import Hexagon from "../assets/images/nepali/Satkon.svg";
+import Aidu from "../assets/images/kannada/Aidu.svg";
+import Camel from "../assets/images/Camel.svg";
+import Read from "../assets/images/kannada/Odu.svg";
+import Medicine from "../assets/images/kannada/Aushadhi.svg";
+import Lotus from "../assets/images/Lotus.svg";
+import Khadga from "../assets/images/kannada/Khadga.svg";
+import Wheat from "../assets/images/kannada/Godhi.svg";
+import Bell from "../assets/images/kannada/Ghante.svg";
+import Chatri from "../assets/images/nepali/Chata.svg";
+import Water from "../assets/images/water.svg";
+import Bow from "../assets/images/nepali/Dhanush.svg";
+import Chamacha from "../assets/images/kannada/Chamacha.svg";
+import Ele from "../assets/images/kannada/Ele.svg";
+import Vana from "../assets/images/kannada/Vana.svg";
+import Bharani from "../assets/images/kannada/Bharani.svg";
 import Box from "../assets/images/kannada/Box.svg";
 import Mother from "../assets/images/kannada/Mother.svg";
-import Greeting from "../assets/images/kannada/Greeting.svg";
-import Word from "../assets/images/kannada/Word.svg";
+import Waterfalls from "../assets/images/nepali/Jharna.svg";
+import Langa from "../assets/images/kannada/Langa.svg";
 import House from "../assets/images/kannada/House.svg";
-import Success from "../assets/images/kannada/Success.svg";
+import Yantra from "../assets/images/kannada/Yantra.svg";
 import Chariot from "../assets/images/nepali/Ratha.svg";
-import Salt from "../assets/images/kannada/Salt.svg";
-import Tree from "../assets/images/kannada/Tree.svg";
+import Sara from "../assets/images/kannada/Sara.svg";
+import Ship from "../assets/images/Ship.svg";
 import Power from "../assets/images/kannada/Power.svg";
-import Sun from "../assets/images/kannada/Sun.svg";
+import Yedi from "../assets/images/kannada/Yedi.svg";
 import Cow from "../assets/images/kannada/Cow.svg";
+import Bale from "../assets/images/kannada/Bale.svg";
+import Topi from "../assets/images/kannada/Topi.svg";
+import Padaka from "../assets/images/kannada/Padaka.svg";
+import Fruits from "../assets/images/Fruits.svg";
 
 import { FlashCardType } from "../containers/flashCardTypes";
 
 export const consonantCards: FlashCardType[] = [
-    { letter: "ಅ", word: "ಅಕ್ಕ", pronunciation: "Akka", image: Akka, translation: "Sister" },
+    { letter: "ಅ", word: "ಅರಸ", pronunciation: "Arasa", image: Arasa, translation: "King" },
     { letter: "ಆ", word: "ಆನೆ", pronunciation: "Āne", image: Elephant, translation: "Elephant" },
     { letter: "ಇ", word: "ಇಲಿ", pronunciation: "Ili", image: Mouse, translation: "Mouse" },
-    { letter: "ಈ", word: "ಈಜು", pronunciation: "Īju", image: Swim, translation: "Swim" },
-    { letter: "ಉ", word: "ಉಂಡೆ", pronunciation: "Uṇḍe", image: Laddu, translation: "Laddu" },
-    { letter: "ಊ", word: "ಊಟ", pronunciation: "Ūṭa", image: Meal, translation: "Meal" },
-    { letter: "ಎ", word: "ಎಲೆ", pronunciation: "Ele", image: Leaf, translation: "Leaf" },
-    { letter: "ಏ", word: "ಏನು", pronunciation: "Ēnu", image: What, translation: "What" },
-    { letter: "ಐ", word: "ಐದು", pronunciation: "Aidu", image: Five, translation: "Five" },
-    { letter: "ಒ", word: "ಒಂಟಿ", pronunciation: "Oṇṭi", image: Alone, translation: "Alone" },
+    { letter: "ಈ", word: "ಈಜು", pronunciation: "Eeju", image: Swim, translation: "Swim" },
+    { letter: "ಉ", word: "ಉಂಗುರ", pronunciation: "Ungura", image: Ungura, translation: "Ring" },
+    { letter: "ಊ", word: "ಊಟ", pronunciation: "Oota", image: Meal, translation: "Meal" },
+    { letter: "ಎ", word: "ಎಲೆ", pronunciation: "Yele", image: Ele, translation: "Leaf" },
+    { letter: "ಏ", word: "ಏಡಿ", pronunciation: "Yedi", image: Yedi, translation: "Crab" },
+    { letter: "ಐ", word: "ಐದು", pronunciation: "Aidu", image: Aidu, translation: "Five" },
+    { letter: "ಒ", word: "ಒಂಟೆ", pronunciation: "Ontey", image: Camel, translation: "Camel" },
     { letter: "ಓ", word: "ಓದು", pronunciation: "Ōdu", image: Read, translation: "Read" },
     { letter: "ಔ", word: "ಔಷಧಿ", pronunciation: "Auṣadhi", image: Medicine, translation: "Medicine" },
-    { letter: "ಕ", word: "ಕಪಿ", pronunciation: "Kapi", image: Monkey, translation: "Monkey" },
-    { letter: "ಖ", word: "ಖಡ್ಗ", pronunciation: "Khaḍga", image: Sword, translation: "Sword" },
+    { letter: "ಅಂ", word: "", pronunciation: "", image: What, translation: "" },
+    { letter: "ಅ:", word: "ಆಹ್ವಾನ", pronunciation: "auhvaana", image: Invitation, translation: "Invitation" },
+    { letter: "ಕ", word: "ಕಮಲ", pronunciation: "Kamala", image: Lotus, translation: "Lotus" },
+    { letter: "ಖ", word: "ಖಡ್ಗ", pronunciation: "Khaḍga", image: Khadga, translation: "Sword" },
     { letter: "ಗ", word: "ಗೋಧಿ", pronunciation: "Gōdhi", image: Wheat, translation: "Wheat" },
     { letter: "ಘ", word: "ಘಂಟೆ", pronunciation: "Ghaṇṭe", image: Bell, translation: "Bell" },
-    { letter: "ಚ", word: "ಚೂರು", pronunciation: "Cūru", image: SmallPiece, translation: "Small piece" },
+    { letter: "ಙ", word: "", pronunciation: "", image: What, translation: "" },
+    { letter: "ಚ", word: "ಚಮಚ", pronunciation: "Chamacha", image: Chamacha, translation: "Spoon" },
+    { letter: "ಛ", word: "ಛತ್ರಿ", pronunciation: "Chatri", image: Chatri, translation: "Umbrella" },
     { letter: "ಜ", word: "ಜಲ", pronunciation: "Jala", image: Water, translation: "Water" },
-    { letter: "ಟ", word: "ಟೋಪಿ", pronunciation: "Ṭōpi", image: Hat, translation: "Hat" },
+    { letter: "ಝ", word: "ಝರಿ", pronunciation: "Jari", image: Waterfalls, translation: "WaterFalls" },
+    { letter: "ಞ", word: "", pronunciation: "", image: What, translation: "" },
+    { letter: "ಟ", word: "ಟೋಪಿ", pronunciation: "Topi", image: Topi, translation: "Hat" },
+    { letter: "ಠ", word: "ಠಕ್ಕ", pronunciation: "Takka", image: Thief, translation: "Thief" },
     { letter: "ಡ", word: "ಡಬ್ಬಿ", pronunciation: "Ḍabbi", image: Box, translation: "Box" },
-    { letter: "ತ", word: "ತಾಯಿ", pronunciation: "Tāyi", image: Mother, translation: "Mother" },
-    { letter: "ನ", word: "ನಮಸ್ಕಾರ", pronunciation: "Namaskāra", image: Greeting, translation: "Greeting" },
-    { letter: "ಪ", word: "ಪದ", pronunciation: "Pada", image: Word, translation: "Word" },
+    { letter: "ತ", word: "ತಾಯಿ", pronunciation: "Tāayi", image: Mother, translation: "Mother" },
+    { letter: "ಥ", word: "ಥಳಥಳ", pronunciation: "Thala", image: Shine, translation: "Mother" },
+    { letter: "ದ", word: "ದನ", pronunciation: "Dhana", image: Cow, translation: "Cow" },
+    { letter: "ಧ", word: "ಧನಸ್ಸು", pronunciation: "Dhanasuu", image: Bow, translation: "Bow" },
+    { letter: "ನ", word: "ನದಿ", pronunciation: "Nadi", image: River, translation: "River" },
+    { letter: "ಪ", word: "ಪದಕ", pronunciation: "Padaka", image: Padaka, translation: "Medal" },
+    { letter: "ಫ", word: "ಫಲ", pronunciation: "Phala", image: Fruits, translation: "Fruit" },
+    { letter: "ಬ", word: "ಬಳೆ", pronunciation: "Bale", image: Bale, translation: "Bangle" },
+    { letter: "ಭ", word: "ಭರಣಿ", pronunciation: "Bharani", image: Bharani, translation: "Jar" },
     { letter: "ಮ", word: "ಮನೆ", pronunciation: "Mane", image: House, translation: "House" },
-    { letter: "ಯ", word: "ಯಶಸ್ಸು", pronunciation: "Yaśassu", image: Success, translation: "Success" },
+    { letter: "ಯ", word: "ಯಂತ್ರ", pronunciation: "Yantra", image: Yantra, translation: "Machine" },
     { letter: "ರ", word: "ರಥ", pronunciation: "Ratha", image: Chariot, translation: "Chariot" },
-    { letter: "ಲ", word: "ಲವಣೆ", pronunciation: "Lavaṇe", image: Salt, translation: "Salt" },
-    { letter: "ವ", word: "ವೃಕ್ಷ", pronunciation: "Vṛkṣa", image: Tree, translation: "Tree" },
-    { letter: "ಶ", word: "ಶಕ್ತಿ", pronunciation: "Śakti", image: Power, translation: "Power" },
-    { letter: "ಸ", word: "ಸೂರ್ಯ", pronunciation: "Sūrya", image: Sun, translation: "Sun" },
-    { letter: "ಹ", word: "ಹಸು", pronunciation: "Hasu", image: Cow, translation: "Cow" },
+    { letter: "ಲ", word: "ಲಂಗ", pronunciation: "Langa", image: Langa, translation: "Skirt" },
+    { letter: "ವ", word: "ವನ", pronunciation: "Vana", image: Vana, translation: "Forest" },
+    { letter: "ಶ", word: "ಶಕ್ತಿ", pronunciation: "Shakti", image: Power, translation: "Power" },
+    { letter: "ಷ", word: "ಷಟ್ಕೋನ", pronunciation: "Shatkona", image: Hexagon, translation: "Hexagon" },
+    { letter: "ಸ", word: "ಸರ", pronunciation: "Sara", image: Sara, translation: "Necklace" },
+    { letter: "ಹ", word: "ಹಡಗು", pronunciation: "Hadagu", image: Ship, translation: "Ship" },
 ];
