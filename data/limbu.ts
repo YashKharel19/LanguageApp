5f39ce4a87f91d0ce010c2b7f601ed4521cc56c9
yash56 <yash@connectex.io>
2025-07-30 15:37:17 -0600

    added limbu script

--- data/limbu.ts
+++ data/limbu.ts
@@ -1,46 +1,63 @@
-import kabu from "../assets/images/Baan.svg";
-import khayekcha from "../assets/images/Baan.svg";
-import nga_k_kwa from "../assets/images/Baan.svg";
-import chatchap from "../assets/images/Baan.svg";
-import pasa from "../assets/images/Baan.svg";
-import Elephant from "../assets/images/Elephant.svg";
-import Pigeon from "../assets/images/nepali/Parewa.svg";
-import numa from "../assets/images/Baan.svg";
-import Ox from "../assets/images/nepali/Goru.svg";
-import Flower from "../assets/images/nepali/Fhul.svg";
-import Eye from "../assets/images/Eye.svg";
-import yuma from "../assets/images/Baan.svg";
-import Lemon from "../assets/images/Lemon.svg";
-import wase_k from "../assets/images/Baan.svg";
-import sakk_ingghnga from "../assets/images/Baan.svg";
-import King from "../assets/images/kannada/Arasa.svg";
+// 📦 Image Imports
+import village from "../assets/images/Village.svg";
+import watermelon from "../assets/images/Watermelon.svg";
+import car from "../assets/images/nepali/Yaan.svg";
+import fish from "../assets/images/Fish.svg";
+import bat from "../assets/images/Bat.svg";
+import notebook from "../assets/images/nepali/Yaan.svg";
+import wallet from "../assets/images/Wallet.svg";
+import mirror from "../assets/images/Mirror.svg";
+import mother from "../assets/images/Mother.svg";
+import tomato from "../assets/images/Tomato.svg";
+import elephant from "../assets/images/Elephant.svg";
+import pigeon from "../assets/images/nepali/Parewa.svg";
+import bowl from "../assets/images/Bowl.svg";
+import nose from "../assets/images/nepali/Naak.svg";
+import fruit from "../assets/images/Fruits.svg";
+import father from "../assets/images/Father.svg";
+import leaf from "../assets/images/Leaf.svg";
+import rice from "../assets/images/rice.svg";
+import flower from "../assets/images/nepali/Fhul.svg";
+import fire from "../assets/images/Fire.svg";
+import night from "../assets/images/Night.svg";
+import goat from "../assets/images/Goat.svg";
+import road from "../assets/images/Road.svg";
+import wind from "../assets/images/Wind.svg";
+import meat from "../assets/images/Meat.svg";
+import earth from "../assets/images/Earth.svg";
+import heart from "../assets/images/Heart.svg";
+import egg from "../assets/images/Egg.svg";
 
 import { FlashCardType } from "../containers/flashCardTypes";
 
-export const consonantCards: FlashCardType[] = [
-    { letter: 'ᤁ', pronunciation: 'kabu', image: kabu, word: 'ᤁᤒᤢ', translation: 'Village' },
-    { letter: 'ᤂ', pronunciation: 'khayekcha', image: khayekcha, word: 'ᤂᤕᤧᤰᤆᤠ', translation: 'Watermelon' },
-    { letter: 'ᤃ', pronunciation: '', image: khayekcha, word: '', translation: '' },
-    { letter: 'ᤄ', pronunciation: '', image: khayekcha, word: '', translation: '' },
-    { letter: 'ᤅ', pronunciation: 'Nga k kwa', image: nga_k_kwa, word: 'ᤅᤧ᤺ᤰᤘᤠ', translation: 'Bat' },
-    { letter: 'ᤆ', pronunciation: 'Chatchap', image: chatchap, word: 'ᤆᤳᤇᤠᤵ', translation: 'Notebook' },
-    { letter: 'ᤇ', pronunciation: 'Pasa', image: pasa, word: 'ᤐᤠᤛᤠ', translation: 'Wallet' },
-    { letter: 'ᤈ', pronunciation: '', image: khayekcha, word: '', translation: '' },
-    { letter: 'ᤋ', pronunciation: 'Takmi ba', image: Elephant, word: 'ᤋᤰᤔᤡ(ᤒᤠ)', translation: 'Elephant' },
-    { letter: 'ᤌ', pronunciation: 'Thangwa', image: Pigeon, word: 'ᤌᤠᤱᤘᤠ᤹', translation: 'Pigeon' },
-    { letter: 'ᤍ', pronunciation: '', image: khayekcha, word: '', translation: '' },
-    { letter: 'ᤎ', pronunciation: '', image: khayekcha, word: '', translation: '' },
-    { letter: 'ᤏ', pronunciation: 'Numa', image: numa, word: 'ᤏᤢᤔᤠ', translation: 'Beautiful' },
-    { letter: 'ᤐ', pronunciation: 'Pakipt', image: Ox, word: 'ᤐᤰᤐᤡᤳ', translation: 'Ox' },
-    { letter: 'ᤑ', pronunciation: 'Phung', image: Flower, word: 'ᤑᤢᤅ', translation: 'Flower' },
-    { letter: 'ᤒ', pronunciation: '', image: khayekcha, word: '', translation: '' },
-    { letter: 'ᤓ', pronunciation: '', image: khayekcha, word: '', translation: '' },
-    { letter: 'ᤔ', pronunciation: 'Mik', image: Eye, word: 'ᤔᤡᤰ', translation: 'Eye' },
-    { letter: 'ᤕ', pronunciation: 'Yuma', image: yuma, word: 'ᤕᤢᤔᤠ', translation: 'Grandmother' },
-    { letter: 'ᤖ', pronunciation: '', image: khayekcha, word: '', translation: '' },
-    { letter: 'ᤗ', pronunciation: 'La rim ba', image: Lemon, word: 'ᤗᤖᤡᤶᤒᤠ', translation: 'Lemon' },
-    { letter: 'ᤘ', pronunciation: 'Wase k', image: wase_k, word: 'ᤘᤛᤣᤁ᤻', translation: 'Silkworm' },
-    { letter: 'ᤙ', pronunciation: '', image: khayekcha, word: '', translation: '' },
-    { letter: 'ᤛ', pronunciation: 'Sakk ingghnga', image: sakk_ingghnga, word: 'ᤛᤰᤀᤡᤱᤄᤱ', translation: 'Letter' },
-    { letter: 'ᤜ', pronunciation: 'Hang', image: King, word: 'ᤜᤠᤅ', translation: 'King' },
+// 🧠 FlashCard Data
+export const limbuCards: FlashCardType[] = [
+    { letter: 'ᤁ', pronunciation: 'kabu', word: 'ᤁᤒᤢ', image: village, translation: 'Village' },
+    { letter: 'ᤂ', pronunciation: 'khayekcha', word: 'ᤂᤕᤧᤰᤆᤠ', image: watermelon, translation: 'Watermelon' },
+    { letter: 'ᤃ', pronunciation: 'gari', word: 'ᤃᤘᤠ᤺', image: car, translation: 'Car' },
+    { letter: 'ᤄ', pronunciation: 'ngasi', word: 'ᤄᤱᤘᤢ', image: fish, translation: 'Fish' },
+    { letter: 'ᤅ', pronunciation: 'nga k kwa', word: 'ᤅᤧ᤺ᤰᤘᤠ', image: bat, translation: 'Bat' },
+    { letter: 'ᤆ', pronunciation: 'chatchap', word: 'ᤆᤳᤇᤠᤵ', image: notebook, translation: 'Notebook' },
+    { letter: 'ᤇ', pronunciation: 'pasa', word: 'ᤇᤠᤛᤠ', image: wallet, translation: 'Wallet' },
+    { letter: 'ᤈ', pronunciation: 'chaplung', word: 'ᤈᤠᤰᤛᤢᤱ', image: mirror, translation: 'Mirror' },
+    { letter: 'ᤉ', pronunciation: 'nyapma', word: 'ᤉᤰᤐᤢ', image: mother, translation: 'Mother' },
+    { letter: 'ᤊ', pronunciation: 'toma', word: 'ᤊᤠᤸᤐᤠ', image: tomato, translation: 'Tomato' },
+    { letter: 'ᤋ', pronunciation: 'takmi ba', word: 'ᤋᤰᤔᤡ(ᤒᤠ)', image: elephant, translation: 'Elephant' },
+    { letter: 'ᤌ', pronunciation: 'thangwa', word: 'ᤌᤠᤱᤘᤠ᤹', image: pigeon, translation: 'Pigeon' },
+    { letter: 'ᤍ', pronunciation: 'dapma', word: 'ᤍᤰᤐᤢ', image: bowl, translation: 'Bowl' },
+    { letter: 'ᤎ', pronunciation: 'nasa', word: 'ᤎᤠ᤺', image: nose, translation: 'Nose' },
+    { letter: 'ᤏ', pronunciation: 'palem', word: 'ᤏᤠᤸᤘᤠ', image: fruit, translation: 'Fruit' },
+    { letter: 'ᤐ', pronunciation: 'pa', word: 'ᤐᤠ', image: father, translation: 'Father' },
+    { letter: 'ᤑ', pronunciation: 'pha', word: 'ᤑᤠ', image: leaf, translation: 'Leaf' },
+    { letter: 'ᤒ', pronunciation: 'ba', word: 'ᤒᤠ', image: rice, translation: 'Rice' },
+    { letter: 'ᤓ', pronunciation: 'bha', word: 'ᤓᤠ', image: flower, translation: 'Flower' },
+    { letter: 'ᤔ', pronunciation: 'ma', word: 'ᤔᤠ', image: fire, translation: 'Fire' },
+    { letter: 'ᤕ', pronunciation: 'ya', word: 'ᤕᤠ', image: night, translation: 'Night' },
+    { letter: 'ᤖ', pronunciation: 'ra', word: 'ᤖᤠ', image: goat, translation: 'Goat' },
+    { letter: 'ᤗ', pronunciation: 'la', word: 'ᤗᤠ', image: road, translation: 'Road' },
+    { letter: 'ᤘ', pronunciation: 'wa', word: 'ᤘᤠ', image: wind, translation: 'Wind' },
+    { letter: 'ᤙ', pronunciation: 'sha', word: 'ᤙᤠ', image: meat, translation: 'Meat' },
+    { letter: 'ᤚ', pronunciation: 'sa', word: 'ᤚᤠ', image: earth, translation: 'Earth' },
+    { letter: 'ᤛ', pronunciation: 'ha', word: 'ᤛᤠ', image: heart, translation: 'Heart' },
+    { letter: 'ᤜ', pronunciation: 'a', word: 'ᤜᤠ', image: egg, translation: 'Egg' },
 ];
