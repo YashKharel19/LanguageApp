4eb0b6773f702e681629a3175f45e23eb50d22d3
yash56 <yash@connectex.io>
2025-11-12 15:33:51 -0700

    gujarati done

--- data/gujarati.ts
+++ data/gujarati.ts
@@ -44,56 +44,147 @@ import Seven from "../assets/images/numbers/7.png";
 import Eight from "../assets/images/numbers/8.png";
 import Nine from "../assets/images/numbers/9.png";
 
+
+// Letter audio imports
+import letter1 from "../assets/sounds/gujarati/letter1.mp3";
+import letter2 from "../assets/sounds/gujarati/letter2.mp3";
+import letter3 from "../assets/sounds/gujarati/letter3.mp3";
+import letter4 from "../assets/sounds/gujarati/letter4.mp3";
+import letter5 from "../assets/sounds/gujarati/letter5.mp3";
+import letter6 from "../assets/sounds/gujarati/letter6.mp3";
+import letter7 from "../assets/sounds/gujarati/letter7.mp3";
+import letter8 from "../assets/sounds/gujarati/letter8.mp3";
+import letter9 from "../assets/sounds/gujarati/letter9.mp3";
+import letter10 from "../assets/sounds/gujarati/letter10.mp3";
+import letter11 from "../assets/sounds/gujarati/letter11.mp3";
+import letter12 from "../assets/sounds/gujarati/letter12.mp3";
+import letter13 from "../assets/sounds/gujarati/letter13.mp3";
+import letter14 from "../assets/sounds/gujarati/letter14.mp3";
+import letter15 from "../assets/sounds/gujarati/letter15.mp3";
+import letter16 from "../assets/sounds/gujarati/letter16.mp3";
+import letter17 from "../assets/sounds/gujarati/letter17.mp3";
+import letter18 from "../assets/sounds/gujarati/letter18.mp3";
+import letter19 from "../assets/sounds/gujarati/letter19.mp3";
+import letter20 from "../assets/sounds/gujarati/letter20.mp3";
+import letter21 from "../assets/sounds/gujarati/letter21.mp3";
+import letter22 from "../assets/sounds/gujarati/letter22.mp3";
+import letter23 from "../assets/sounds/gujarati/letter23.mp3";
+import letter24 from "../assets/sounds/gujarati/letter24.mp3";
+import letter25 from "../assets/sounds/gujarati/letter25.mp3";
+import letter26 from "../assets/sounds/gujarati/letter26.mp3";
+import letter27 from "../assets/sounds/gujarati/letter27.mp3";
+import letter28 from "../assets/sounds/gujarati/letter28.mp3";
+import letter29 from "../assets/sounds/gujarati/letter29.mp3";
+import letter30 from "../assets/sounds/gujarati/letter30.mp3";
+import letter31 from "../assets/sounds/gujarati/letter31.mp3";
+import letter32 from "../assets/sounds/gujarati/letter32.mp3";
+import letter33 from "../assets/sounds/gujarati/letter33.mp3";
+import letter34 from "../assets/sounds/gujarati/letter34.mp3";
+import letter35 from "../assets/sounds/gujarati/letter35.mp3";
+import letter36 from "../assets/sounds/gujarati/letter36.mp3";
+
+
+// Word audio imports
+import Word1 from "../assets/sounds/gujarati/word1.mp3";
+import Word2 from "../assets/sounds/gujarati/word2.mp3";
+import Word3 from "../assets/sounds/gujarati/word3.mp3";
+import Word4 from "../assets/sounds/gujarati/word4.mp3";
+import Word5 from "../assets/sounds/gujarati/word5.mp3";
+import Word6 from "../assets/sounds/gujarati/word6.mp3";
+import Word7 from "../assets/sounds/gujarati/word7.mp3";
+import Word8 from "../assets/sounds/gujarati/word8.mp3";
+import Word9 from "../assets/sounds/gujarati/word9.mp3";
+import Word10 from "../assets/sounds/gujarati/letter10.mp3";
+import Word11 from "../assets/sounds/gujarati/word11.mp3";
+import Word12 from "../assets/sounds/gujarati/word12.mp3";
+import Word13 from "../assets/sounds/gujarati/word13.mp3";
+import Word14 from "../assets/sounds/gujarati/word14.mp3";
+import Word15 from "../assets/sounds/gujarati/word15.mp3";
+import Word16 from "../assets/sounds/gujarati/word16.mp3";
+import Word17 from "../assets/sounds/gujarati/word17.mp3";
+import Word18 from "../assets/sounds/gujarati/word18.mp3";
+import Word19 from "../assets/sounds/gujarati/word19.mp3";
+import Word20 from "../assets/sounds/gujarati/word20.mp3";
+import Word21 from "../assets/sounds/gujarati/word21.mp3";
+import Word22 from "../assets/sounds/gujarati/word22.mp3";
+import Word23 from "../assets/sounds/gujarati/word23.mp3";
+import Word24 from "../assets/sounds/gujarati/word24.mp3";
+import Word25 from "../assets/sounds/gujarati/word25.mp3";
+import Word26 from "../assets/sounds/gujarati/word26.mp3";
+import Word27 from "../assets/sounds/gujarati/word27.mp3";
+import Word28 from "../assets/sounds/gujarati/word28.mp3";
+import Word29 from "../assets/sounds/gujarati/word29.mp3";
+import Word30 from "../assets/sounds/gujarati/word30.mp3";
+import Word31 from "../assets/sounds/gujarati/word31.mp3";
+import Word32 from "../assets/sounds/gujarati/word32.mp3";
+import Word33 from "../assets/sounds/gujarati/word33.mp3";
+import Word34 from "../assets/sounds/gujarati/word34.mp3";
+import Word35 from "../assets/sounds/gujarati/word35.mp3";
+import Word36 from "../assets/sounds/gujarati/word36.mp3";
+
+// Audio imports for Gujarati numbers
+import num0 from "../assets/sounds/gujarati/zero.mp3";
+import num1 from "../assets/sounds/gujarati/one.mp3";
+import num2 from "../assets/sounds/gujarati/two.mp3";
+import num3 from "../assets/sounds/gujarati/three.mp3";
+import num4 from "../assets/sounds/gujarati/four.mp3";
+import num5 from "../assets/sounds/gujarati/five.mp3";
+import num6 from "../assets/sounds/gujarati/six.mp3";
+import num7 from "../assets/sounds/gujarati/seven.mp3";
+import num8 from "../assets/sounds/gujarati/eight.mp3";
+import num9 from "../assets/sounds/gujarati/nine.mp3";
+
 import { FlashCardType } from "../containers/flashCardTypes";
 
 export const consonantCards: FlashCardType[] = [
-    { letter: "ક", letterPronunciation: "ka", word: "કમળ", pronunciation: "Kamal", image: Lotus, translation: "Lotus flower" },
-    { letter: "ખ", letterPronunciation: "kha", word: "ખગોળ", pronunciation: "Khagol", image: Telescope, translation: "Telescope" },
-    { letter: "ગ", letterPronunciation: "ga", word: "ગાંઠ", pronunciation: "Gaanth", image: Knot, translation: "Knot" },
-    { letter: "ઘ", letterPronunciation: "gha", word: "ઘોડો", pronunciation: "Horse", image: Horse, translation: "Horse" },
-    { letter: "ઙ", letterPronunciation: "nga", word: "ઙણ", pronunciation: "Ṅa", image: What, translation: "" },
-    { letter: "ચ", letterPronunciation: "cha", word: "ચિત્ર", pronunciation: "Chitra", image: Picture, translation: "Picture" },
-    { letter: "છ", letterPronunciation: "Chha", word: "છત્રી", pronunciation: "Chhatrī", image: Umbrella, translation: "Umbrella" },
-    { letter: "જ", letterPronunciation: "ja", word: "જલ", pronunciation: "Jala", image: Water, translation: "Water" },
-    { letter: "ઝ", letterPronunciation: "Jha", word: "ઝરણા", pronunciation: "jhar-naa", image: Waterfall, translation: "Waterfall" },
-    { letter: "ઞ", letterPronunciation: "Nya", word: "", pronunciation: "", image: What, translation: "" },
-    { letter: "ટ", letterPronunciation: "Ta", word: "ટમેટા", pronunciation: "Tameta", image: Tomato, translation: "Tomato" },
-    { letter: "ઠ", letterPronunciation: "Tha", word: "ઠંડી", pronunciation: "Thandi", image: Cold, translation: "Cold" },
-    { letter: "ડ", letterPronunciation: "Da", word: "ડોલ", pronunciation: "Dol", image: Bucket, translation: "Bucket" },
-    { letter: "ઢ", letterPronunciation: "Dha", word: "ઢોંકો", pronunciation: "Dhol", image: Drum, translation: "Drum" },
-    { letter: "ણ", letterPronunciation: "Na", word: "ણવ", pronunciation: "Shramann", image: What, translation: "" },
-    { letter: "ત", letterPronunciation: "ta", word: "તલવાર", pronunciation: "Talvar", image: Sword, translation: "Sword" },
-    { letter: "થ", letterPronunciation: "tha", word: "થાળી", pronunciation: "Thali", image: Plate, translation: "Plate" },
-    { letter: "દ", letterPronunciation: "da", word: "દૂધ", pronunciation: "Dudh", image: Milk, translation: "Milk" },
-    { letter: "ધ", letterPronunciation: "dha", word: "ધન", pronunciation: "Dhan", image: Wealth, translation: "Wealth" },
-    { letter: "ન", letterPronunciation: "na", word: "નારીયળ", pronunciation: "Naa-ree-yal", image: Coconut, translation: "Coconut" },
-    { letter: "પ", letterPronunciation: "pa", word: "પંખી", pronunciation: "Pankhi", image: Bird, translation: "Bird" },
-    { letter: "ફ", letterPronunciation: "pha", word: "ફૂલ", pronunciation: "Phul", image: Flower, translation: "Flower" },
-    { letter: "બ", letterPronunciation: "ba", word: "બાળક", pronunciation: "Baa-dak", image: Child, translation: "Child" },
-    { letter: "ભ", letterPronunciation: "bha", word: "ભક્ત", pronunciation: "Bhakta", image: Devotee, translation: "Devotee" },
-    { letter: "મ", letterPronunciation: "ma", word: "મકાન", pronunciation: "Makan", image: House, translation: "House" },
-    { letter: "ય", letterPronunciation: "ya", word: "યાત્રા", pronunciation: "Yatra", image: Journey, translation: "Journey" },
-    { letter: "ર", letterPronunciation: "ra", word: "રીંછ", pronunciation: "Reenchh", image: Bear, translation: "Bear" },
-    { letter: "લ", letterPronunciation: "la", word: "લસણ", pronunciation: "Lasan", image: Garlic, translation: "Garlic" },
-    { letter: "વ", letterPronunciation: "va", word: "વૃત્તિ", pronunciation: "Vruti", image: Occupation, translation: "Occupation" },
-    { letter: "શ", letterPronunciation: "sha", word: "શરમ", pronunciation: "Sharam", image: Shame, translation: "Shame" },
-    { letter: "ષ", letterPronunciation: "Sha", word: "ષટક", pronunciation: "Shatak", image: Hexagon, translation: "Hexagon" },
-    { letter: "સ", letterPronunciation: "Sa", word: "સૂર્ય", pronunciation: "Surya", image: Sun, translation: "Sun" },
-    { letter: "હ", letterPronunciation: "ha", word: "હાથી", pronunciation: "Haathi", image: Elephant, translation: "Elephant" },
-    { letter: "ળ", letterPronunciation: "la", word: "ળણ", pronunciation: "", image: What, translation: "" },
-    { letter: "ક્ષ", letterPronunciation: "Ksha", word: "ક્ષતિ", pronunciation: "Kshati", image: Damage, translation: "Damage" },
-    { letter: "જ્ઞ", letterPronunciation: "gya", word: "જ્ઞાની", pronunciation: "Gyaani", image: Wise, translation: "Wise person" },
+    { letter: "ક", word: "કમળ", pronunciation: "Kamal", letterPronunciation: "ka", image: Lotus, translation: "Lotus flower", audioLetter: letter1, audioWord: Word1 },
+    { letter: "ખ", word: "ખગોળ", pronunciation: "Khagol", letterPronunciation: "kha", image: Telescope, translation: "Telescope", audioLetter: letter2, audioWord: Word2 },
+    { letter: "ગ", word: "ગાંઠ", pronunciation: "Gaanth", letterPronunciation: "ga", image: Knot, translation: "Knot", audioLetter: letter3, audioWord: Word3 },
+    { letter: "ઘ", word: "ઘોડો", pronunciation: "Ghodo", letterPronunciation: "gha", image: Horse, translation: "Horse", audioLetter: letter4, audioWord: Word4 },
+    { letter: "ઙ", word: "ઙણ", pronunciation: "Nga", letterPronunciation: "nga", image: What, translation: "", audioLetter: letter5, audioWord: Word5 },
+    { letter: "ચ", word: "ચિત્ર", pronunciation: "Chitra", letterPronunciation: "cha", image: Picture, translation: "Picture", audioLetter: letter6, audioWord: Word6 },
+    { letter: "છ", word: "છત્રી", pronunciation: "Chhatri", letterPronunciation: "chha", image: Umbrella, translation: "Umbrella", audioLetter: letter7, audioWord: Word7 },
+    { letter: "જ", word: "જલ", pronunciation: "Jal", letterPronunciation: "ja", image: Water, translation: "Water", audioLetter: letter8, audioWord: Word8 },
+    { letter: "ઝ", word: "ઝરણા", pronunciation: "Jharna", letterPronunciation: "jha", image: Waterfall, translation: "Waterfall", audioLetter: letter9, audioWord: Word9 },
+    { letter: "ઞ", word: "", pronunciation: "", letterPronunciation: "nya", image: What, translation: "", audioLetter: letter10, audioWord: Word10 },
+    { letter: "ટ", word: "ટમેટા", pronunciation: "Tameta", letterPronunciation: "ta", image: Tomato, translation: "Tomato", audioLetter: letter11, audioWord: Word11 },
+    { letter: "ઠ", word: "ઠંડી", pronunciation: "Thandi", letterPronunciation: "tha", image: Cold, translation: "Cold", audioLetter: letter12, audioWord: Word12 },
+    { letter: "ડ", word: "ડોલ", pronunciation: "Dol", letterPronunciation: "da", image: Bucket, translation: "Bucket", audioLetter: letter13, audioWord: Word13 },
+    { letter: "ઢ", word: "ઢોંકો", pronunciation: "Dhol", letterPronunciation: "dha", image: Drum, translation: "Drum", audioLetter: letter14, audioWord: Word14 },
+    { letter: "ણ", word: "ણવ", pronunciation: "Naan", letterPronunciation: "na", image: What, translation: "", audioLetter: letter15, audioWord: Word15 },
+    { letter: "ત", word: "તલવાર", pronunciation: "Talvar", letterPronunciation: "ta", image: Sword, translation: "Sword", audioLetter: letter16, audioWord: Word16 },
+    { letter: "થ", word: "થાળી", pronunciation: "Thali", letterPronunciation: "tha", image: Plate, translation: "Plate", audioLetter: letter17, audioWord: Word17 },
+    { letter: "દ", word: "દૂધ", pronunciation: "Dudh", letterPronunciation: "da", image: Milk, translation: "Milk", audioLetter: letter18, audioWord: Word18 },
+    { letter: "ધ", word: "ધન", pronunciation: "Dhan", letterPronunciation: "dha", image: Wealth, translation: "Wealth", audioLetter: letter19, audioWord: Word19 },
+    { letter: "ન", word: "નારીયળ", pronunciation: "Naariyal", letterPronunciation: "na", image: Coconut, translation: "Coconut", audioLetter: letter20, audioWord: Word20 },
+    { letter: "પ", word: "પંખી", pronunciation: "Pankhi", letterPronunciation: "pa", image: Bird, translation: "Bird", audioLetter: letter21, audioWord: Word21 },
+    { letter: "ફ", word: "ફૂલ", pronunciation: "Phul", letterPronunciation: "pha", image: Flower, translation: "Flower", audioLetter: letter22, audioWord: Word22 },
+    { letter: "બ", word: "બાળક", pronunciation: "Baalk", letterPronunciation: "ba", image: Child, translation: "Child", audioLetter: letter23, audioWord: Word23 },
+    { letter: "ભ", word: "ભક્ત", pronunciation: "Bhakta", letterPronunciation: "bha", image: Devotee, translation: "Devotee", audioLetter: letter24, audioWord: Word24 },
+    { letter: "મ", word: "મકાન", pronunciation: "Makan", letterPronunciation: "ma", image: House, translation: "House", audioLetter: letter25, audioWord: Word25 },
+    { letter: "ય", word: "યાત્રા", pronunciation: "Yatra", letterPronunciation: "ya", image: Journey, translation: "Journey", audioLetter: letter26, audioWord: Word26 },
+    { letter: "ર", word: "રીંછ", pronunciation: "Reenchh", letterPronunciation: "ra", image: Bear, translation: "Bear", audioLetter: letter27, audioWord: Word27 },
+    { letter: "લ", word: "લસણ", pronunciation: "Lasan", letterPronunciation: "la", image: Garlic, translation: "Garlic", audioLetter: letter28, audioWord: Word28 },
+    { letter: "વ", word: "વૃત્તિ", pronunciation: "Vruti", letterPronunciation: "va", image: Occupation, translation: "Occupation", audioLetter: letter29, audioWord: Word29 },
+    { letter: "શ", word: "શરમ", pronunciation: "Sharam", letterPronunciation: "sha", image: Shame, translation: "Shame", audioLetter: letter30, audioWord: Word30 },
+    { letter: "ષ", word: "ષટક", pronunciation: "Shatak", letterPronunciation: "sha", image: Hexagon, translation: "Hexagon", audioLetter: letter31, audioWord: Word31 },
+    { letter: "સ", word: "સૂર્ય", pronunciation: "Surya", letterPronunciation: "sa", image: Sun, translation: "Sun", audioLetter: letter32, audioWord: Word32 },
+    { letter: "હ", word: "હાથી", pronunciation: "Haathi", letterPronunciation: "ha", image: Elephant, translation: "Elephant", audioLetter: letter33, audioWord: Word33 },
+    { letter: "ળ", word: "ળણ", pronunciation: "Lan", letterPronunciation: "la", image: What, translation: "", audioLetter: letter34, audioWord: Word34 },
+    { letter: "ક્ષ", word: "ક્ષતિ", pronunciation: "Kshati", letterPronunciation: "ksha", image: Damage, translation: "Damage", audioLetter: letter35, audioWord: Word35 },
+    { letter: "જ્ઞ", word: "જ્ઞાની", pronunciation: "Gyaani", letterPronunciation: "gya", image: Wise, translation: "Wise person", audioLetter: letter36, audioWord: Word36 },
 ];
 
+// Number cards with audio mapping
 export const numberCards: FlashCardType[] = [
-    { letter: "૦", word: "શૂન્ય", letterPronunciation: "shoon-ya", pronunciation: "shoon-ya", image: Zero, translation: "Zero" },
-    { letter: "૧", word: "એક", letterPronunciation: "ek", pronunciation: "ek", image: One, translation: "One" },
-    { letter: "૨", word: "બે", letterPronunciation: "beh", pronunciation: "beh", image: Two, translation: "Two" },
-    { letter: "૩", word: "ત્રણ", letterPronunciation: "tran", pronunciation: "tran", image: Three, translation: "Three" },
-    { letter: "૪", word: "ચાર", letterPronunciation: "chaar", pronunciation: "chaar", image: Four, translation: "Four" },
-    { letter: "૫", word: "પાંચ", letterPronunciation: "paanch", pronunciation: "paanch", image: Five, translation: "Five" },
-    { letter: "૬", word: "છ", letterPronunciation: "chhah", pronunciation: "chhah", image: Six, translation: "Six" },
-    { letter: "૭", word: "સાત", letterPronunciation: "saat", pronunciation: "saat", image: Seven, translation: "Seven" },
-    { letter: "૮", word: "આઠ", letterPronunciation: "aath", pronunciation: "aath", image: Eight, translation: "Eight" },
-    { letter: "૯", word: "નવ", letterPronunciation: "nav", pronunciation: "nav", image: Nine, translation: "Nine" },
+    { letter: "૦", word: "શૂન્ય", letterPronunciation: "shoon-ya", pronunciation: "shoon-ya", image: Zero, translation: "Zero", audioWord: num0, audioLetter: num0 },
+    { letter: "૧", word: "એક", letterPronunciation: "ek", pronunciation: "ek", image: One, translation: "One", audioWord: num1, audioLetter: num1 },
+    { letter: "૨", word: "બે", letterPronunciation: "beh", pronunciation: "beh", image: Two, translation: "Two", audioWord: num2, audioLetter: num2 },
+    { letter: "૩", word: "ત્રણ", letterPronunciation: "tran", pronunciation: "tran", image: Three, translation: "Three", audioWord: num3, audioLetter: num3 },
+    { letter: "૪", word: "ચાર", letterPronunciation: "chaar", pronunciation: "chaar", image: Four, translation: "Four", audioWord: num4, audioLetter: num4 },
+    { letter: "૫", word: "પાંચ", letterPronunciation: "paanch", pronunciation: "paanch", image: Five, translation: "Five", audioWord: num5, audioLetter: num5 },
+    { letter: "૬", word: "છ", letterPronunciation: "chhah", pronunciation: "chhah", image: Six, translation: "Six", audioWord: num6, audioLetter: num6 },
+    { letter: "૭", word: "સાત", letterPronunciation: "saat", pronunciation: "saat", image: Seven, translation: "Seven", audioWord: num7, audioLetter: num7 },
+    { letter: "૮", word: "આઠ", letterPronunciation: "aath", pronunciation: "aath", image: Eight, translation: "Eight", audioWord: num8, audioLetter: num8 },
+    { letter: "૯", word: "નવ", letterPronunciation: "nav", pronunciation: "nav", image: Nine, translation: "Nine", audioWord: num9, audioLetter: num9 },
 ];
