f9e535780297f91ce17134e8d36679cf4c80f767
yash56 <yash@connectex.io>
2025-11-17 10:26:39 -0700

    portugue voice mapped

--- data/portuguese.ts
+++ data/portuguese.ts
@@ -36,47 +36,114 @@ import Seven from "../assets/images/numbers/7.png";
 import Eight from "../assets/images/numbers/8.png";
 import Nine from "../assets/images/numbers/9.png";
 
+import letter1 from "../assets/sounds/portuguese/letter1.mp3";
+import letter2 from "../assets/sounds/portuguese/letter2.mp3";
+import letter3 from "../assets/sounds/portuguese/letter3.mp3";
+import letter4 from "../assets/sounds/portuguese/letter4.mp3";
+import letter5 from "../assets/sounds/portuguese/letter5.mp3";
+import letter6 from "../assets/sounds/portuguese/letter6.mp3";
+import letter7 from "../assets/sounds/portuguese/letter7.mp3";
+import letter8 from "../assets/sounds/portuguese/letter8.mp3";
+import letter9 from "../assets/sounds/portuguese/letter9.mp3";
+import letter10 from "../assets/sounds/portuguese/letter10.mp3";
+import letter11 from "../assets/sounds/portuguese/letter11.mp3";
+import letter12 from "../assets/sounds/portuguese/letter12.mp3";
+import letter13 from "../assets/sounds/portuguese/letter13.mp3";
+import letter14 from "../assets/sounds/portuguese/letter14.mp3";
+import letter15 from "../assets/sounds/portuguese/letter15.mp3";
+import letter16 from "../assets/sounds/portuguese/letter16.mp3";
+import letter17 from "../assets/sounds/portuguese/letter17.mp3";
+import letter18 from "../assets/sounds/portuguese/letter18.mp3";
+import letter19 from "../assets/sounds/portuguese/letter19.mp3";
+import letter20 from "../assets/sounds/portuguese/letter20.mp3";
+import letter21 from "../assets/sounds/portuguese/letter21.mp3";
+import letter22 from "../assets/sounds/portuguese/letter22.mp3";
+import letter23 from "../assets/sounds/portuguese/letter23.mp3";
+import letter24 from "../assets/sounds/portuguese/letter24.mp3";
+import letter25 from "../assets/sounds/portuguese/letter25.mp3";
+import letter26 from "../assets/sounds/portuguese/letter26.mp3";
+
+import word1 from "../assets/sounds/portuguese/word1.mp3";
+import word2 from "../assets/sounds/portuguese/word2.mp3";
+import word3 from "../assets/sounds/portuguese/word3.mp3";
+import word4 from "../assets/sounds/portuguese/word4.mp3";
+import word5 from "../assets/sounds/portuguese/word5.mp3";
+import word6 from "../assets/sounds/portuguese/word6.mp3";
+import word7 from "../assets/sounds/portuguese/word7.mp3";
+import word8 from "../assets/sounds/portuguese/word8.mp3";
+import word9 from "../assets/sounds/portuguese/word9.mp3";
+import word10 from "../assets/sounds/portuguese/word10.mp3";
+import word11 from "../assets/sounds/portuguese/word11.mp3";
+import word12 from "../assets/sounds/portuguese/word12.mp3";
+import word13 from "../assets/sounds/portuguese/word13.mp3";
+import word14 from "../assets/sounds/portuguese/word14.mp3";
+import word15 from "../assets/sounds/portuguese/word15.mp3";
+import word16 from "../assets/sounds/portuguese/word16.mp3";
+import word17 from "../assets/sounds/portuguese/word17.mp3";
+import word18 from "../assets/sounds/portuguese/word18.mp3";
+import word19 from "../assets/sounds/portuguese/word19.mp3";
+import word20 from "../assets/sounds/portuguese/word20.mp3";
+import word21 from "../assets/sounds/portuguese/word21.mp3";
+import word22 from "../assets/sounds/portuguese/word22.mp3";
+import word23 from "../assets/sounds/portuguese/word23.mp3";
+import word24 from "../assets/sounds/portuguese/word24.mp3";
+import word25 from "../assets/sounds/portuguese/word25.mp3";
+import word26 from "../assets/sounds/portuguese/word26.mp3";
+
+import num0 from "../assets/sounds/portuguese/zero.mp3";
+import num1 from "../assets/sounds/portuguese/one.mp3";
+import num2 from "../assets/sounds/portuguese/two.mp3";
+import num3 from "../assets/sounds/portuguese/three.mp3";
+import num4 from "../assets/sounds/portuguese/four.mp3";
+import num5 from "../assets/sounds/portuguese/five.mp3";
+import num6 from "../assets/sounds/portuguese/six.mp3";
+import num7 from "../assets/sounds/portuguese/seven.mp3";
+import num8 from "../assets/sounds/portuguese/eight.mp3";
+import num9 from "../assets/sounds/portuguese/nine.mp3";
+
+
 import { FlashCardType } from "../containers/flashCardTypes";
 
 export const consonantCards: FlashCardType[] = [
-    { letter: "A", word: "Abelha", letterPronunciation: "ah", pronunciation: "ah-BAY-lyah", image: Bee, translation: "Bee" },
-    { letter: "B", word: "Bola", letterPronunciation: "bê", pronunciation: "BOH-lah", image: Ball, translation: "Ball" },
-    { letter: "C", word: "Carro", letterPronunciation: "cê", pronunciation: "car", image: Car, translation: "Car" },
-    { letter: "D", word: "Dado", letterPronunciation: "dê", pronunciation: "dah-hoo", image: Dice, translation: "Dice" },
-    { letter: "E", word: "Elefante", letterPronunciation: "é", pronunciation: "eh-leh-FAHN-chee", image: Elephant, translation: "Elephant" },
-    { letter: "F", word: "Foca", letterPronunciation: "efe", pronunciation: "FOH-kah", image: Seal, translation: "Seal" },
-    { letter: "G", word: "Gato", letterPronunciation: "gê", pronunciation: "GAH-too", image: Cat, translation: "Cat" },
-    { letter: "H", word: "Hipopótamo", letterPronunciation: "agá", pronunciation: "ee-poh-POH-tah-moo", image: Hippopotamus, translation: "Hippopotamus" },
-    { letter: "I", word: "Iguana", letterPronunciation: "i", pronunciation: "ee-GWAH-nah", image: Iguana, translation: "Iguana" },
-    { letter: "J", word: "Jacaré", letterPronunciation: "jóta", pronunciation: "zha-kah-REH", image: Alligator, translation: "Alligator" },
-    { letter: "K", word: "Karatê", letterPronunciation: "ká", pronunciation: "kah-rah-TEH", image: Karate, translation: "Karate" },
-    { letter: "L", word: "Lápis", letterPronunciation: "éle", pronunciation: "LAH-pees", image: Pencil, translation: "Pencil" },
-    { letter: "M", word: "Maçã", letterPronunciation: "ême", pronunciation: "mah-SAHN", image: Apple, translation: "Apple" },
-    { letter: "N", word: "Nuvem", letterPronunciation: "êne", pronunciation: "NOO-vayn", image: Cloud, translation: "Cloud" },
-    { letter: "O", word: "Ovelha", letterPronunciation: "ó", pronunciation: "oh-VEH-lyah", image: Sheep, translation: "Sheep" },
-    { letter: "P", word: "Pato", letterPronunciation: "pê", pronunciation: "PAH-too", image: Duck, translation: "Duck" },
-    { letter: "Q", word: "Queijo", letterPronunciation: "quê", pronunciation: "KAY-zhoo", image: Cheese, translation: "Cheese" },
-    { letter: "R", word: "Rato", letterPronunciation: "érre", pronunciation: "HAH-too", image: Mouse, translation: "Mouse" },
-    { letter: "S", word: "Serpente", letterPronunciation: "ésse", pronunciation: "snake", image: Snake, translation: "Snake" },
-    { letter: "T", word: "Tubarão", letterPronunciation: "tê", pronunciation: "too-bah-HOWN", image: Shark, translation: "Shark" },
-    { letter: "U", word: "Uva", letterPronunciation: "u", pronunciation: "OO-vah", image: Grape, translation: "Grape" },
-    { letter: "V", word: "Vaca", letterPronunciation: "vê", pronunciation: "VAH-kah", image: Cow, translation: "Cow" },
-    { letter: "W", word: "Waffle", letterPronunciation: "dáblio", pronunciation: "ˈwäfəl", image: Waffle, translation: "Waffle" },
-    { letter: "X", word: "Xícara", letterPronunciation: "xis", pronunciation: "SHEE-kah-rah", image: Cup, translation: "Cup" },
-    { letter: "Y", word: "Youtube", letterPronunciation: "ípsilon", pronunciation: "yoo-TOO-bee", image: Youtube, translation: "Youtube" },
-    { letter: "Z", word: "Zebra", letterPronunciation: "zê", pronunciation: "ZEH-brah", image: Zebra, translation: "Zebra" }
+    { letter: "A", word: "Abelha", letterPronunciation: "ah", pronunciation: "ah-BAY-lyah", image: Bee, translation: "Bee", audioLetter: letter1, audioWord: word1 },
+    { letter: "B", word: "Bola", letterPronunciation: "bê", pronunciation: "BOH-lah", image: Ball, translation: "Ball", audioLetter: letter2, audioWord: word2 },
+    { letter: "C", word: "Carro", letterPronunciation: "cê", pronunciation: "car", image: Car, translation: "Car", audioLetter: letter3, audioWord: word3 },
+    { letter: "D", word: "Dado", letterPronunciation: "dê", pronunciation: "dah-hoo", image: Dice, translation: "Dice", audioLetter: letter4, audioWord: word4 },
+    { letter: "E", word: "Elefante", letterPronunciation: "é", pronunciation: "eh-leh-FAHN-chee", image: Elephant, translation: "Elephant", audioLetter: letter5, audioWord: word5 },
+    { letter: "F", word: "Foca", letterPronunciation: "efe", pronunciation: "FOH-kah", image: Seal, translation: "Seal", audioLetter: letter6, audioWord: word6 },
+    { letter: "G", word: "Gato", letterPronunciation: "gê", pronunciation: "GAH-too", image: Cat, translation: "Cat", audioLetter: letter7, audioWord: word7 },
+    { letter: "H", word: "Hipopótamo", letterPronunciation: "agá", pronunciation: "ee-poh-POH-tah-moo", image: Hippopotamus, translation: "Hippopotamus", audioLetter: letter8, audioWord: word8 },
+    { letter: "I", word: "Iguana", letterPronunciation: "i", pronunciation: "ee-GWAH-nah", image: Iguana, translation: "Iguana", audioLetter: letter9, audioWord: word9 },
+    { letter: "J", word: "Jacaré", letterPronunciation: "jóta", pronunciation: "zha-kah-REH", image: Alligator, translation: "Alligator", audioLetter: letter10, audioWord: word10 },
+    { letter: "K", word: "Karatê", letterPronunciation: "ká", pronunciation: "kah-rah-TEH", image: Karate, translation: "Karate", audioLetter: letter11, audioWord: word11 },
+    { letter: "L", word: "Lápis", letterPronunciation: "éle", pronunciation: "LAH-pees", image: Pencil, translation: "Pencil", audioLetter: letter12, audioWord: word12 },
+    { letter: "M", word: "Maçã", letterPronunciation: "ême", pronunciation: "mah-SAHN", image: Apple, translation: "Apple", audioLetter: letter13, audioWord: word13 },
+    { letter: "N", word: "Nuvem", letterPronunciation: "êne", pronunciation: "NOO-vayn", image: Cloud, translation: "Cloud", audioLetter: letter14, audioWord: word14 },
+    { letter: "O", word: "Ovelha", letterPronunciation: "ó", pronunciation: "oh-VEH-lyah", image: Sheep, translation: "Sheep", audioLetter: letter15, audioWord: word15 },
+    { letter: "P", word: "Pato", letterPronunciation: "pê", pronunciation: "PAH-too", image: Duck, translation: "Duck", audioLetter: letter16, audioWord: word16 },
+    { letter: "Q", word: "Queijo", letterPronunciation: "quê", pronunciation: "KAY-zhoo", image: Cheese, translation: "Cheese", audioLetter: letter17, audioWord: word17 },
+    { letter: "R", word: "Rato", letterPronunciation: "érre", pronunciation: "HAH-too", image: Mouse, translation: "Mouse", audioLetter: letter18, audioWord: word18 },
+    { letter: "S", word: "Serpente", letterPronunciation: "ésse", pronunciation: "snake", image: Snake, translation: "Snake", audioLetter: letter19, audioWord: word19 },
+    { letter: "T", word: "Tubarão", letterPronunciation: "tê", pronunciation: "too-bah-HOWN", image: Shark, translation: "Shark", audioLetter: letter20, audioWord: word20 },
+    { letter: "U", word: "Uva", letterPronunciation: "u", pronunciation: "OO-vah", image: Grape, translation: "Grape", audioLetter: letter21, audioWord: word21 },
+    { letter: "V", word: "Vaca", letterPronunciation: "vê", pronunciation: "VAH-kah", image: Cow, translation: "Cow", audioLetter: letter22, audioWord: word22 },
+    { letter: "W", word: "Waffle", letterPronunciation: "dáblio", pronunciation: "ˈwäfəl", image: Waffle, translation: "Waffle", audioLetter: letter23, audioWord: word23 },
+    { letter: "X", word: "Xícara", letterPronunciation: "xis", pronunciation: "SHEE-kah-rah", image: Cup, translation: "Cup", audioLetter: letter24, audioWord: word24 },
+    { letter: "Y", word: "Youtube", letterPronunciation: "ípsilon", pronunciation: "yoo-TOO-bee", image: Youtube, translation: "Youtube", audioLetter: letter25, audioWord: word25 },
+    { letter: "Z", word: "Zebra", letterPronunciation: "zê", pronunciation: "ZEH-brah", image: Zebra, translation: "Zebra", audioLetter: letter26, audioWord: word26 }
 ];
 
 
+
 export const numberCards: FlashCardType[] = [
-    { letter: "0", word: "zero", letterPronunciation: "zeh-roo", pronunciation: "ZEH-roo", image: Zero, translation: "Zero" },
-    { letter: "1", word: "um", letterPronunciation: "oom", pronunciation: "OOM", image: One, translation: "One" },
-    { letter: "2", word: "dois", letterPronunciation: "doysh", pronunciation: "DOYSH", image: Two, translation: "Two" },
-    { letter: "3", word: "três", letterPronunciation: "trehz", pronunciation: "TREHZ", image: Three, translation: "Three" },
-    { letter: "4", word: "quatro", letterPronunciation: "KWA-troh", pronunciation: "KWA-troh", image: Four, translation: "Four" },
-    { letter: "5", word: "cinco", letterPronunciation: "SEEN-koo", pronunciation: "SEEN-koo", image: Five, translation: "Five" },
-    { letter: "6", word: "seis", letterPronunciation: "saysh", pronunciation: "SAYSH", image: Six, translation: "Six" },
-    { letter: "7", word: "sete", letterPronunciation: "SEH-chee", pronunciation: "SEH-chee", image: Seven, translation: "Seven" },
-    { letter: "8", word: "oito", letterPronunciation: "OY-too", pronunciation: "OY-too", image: Eight, translation: "Eight" },
-    { letter: "9", word: "nove", letterPronunciation: "NOH-vee", pronunciation: "NOH-vee", image: Nine, translation: "Nine" },
+    { letter: "0", word: "zero", letterPronunciation: "zeh-roo", pronunciation: "ZEH-roo", image: Zero, translation: "Zero", audioLetter: num0, audioWord: num0 },
+    { letter: "1", word: "um", letterPronunciation: "oom", pronunciation: "OOM", image: One, translation: "One", audioLetter: num1, audioWord: num1 },
+    { letter: "2", word: "dois", letterPronunciation: "doysh", pronunciation: "DOYSH", image: Two, translation: "Two", audioLetter: num2, audioWord: num2 },
+    { letter: "3", word: "três", letterPronunciation: "trehz", pronunciation: "TREHZ", image: Three, translation: "Three", audioLetter: num3, audioWord: num3 },
+    { letter: "4", word: "quatro", letterPronunciation: "KWA-troh", pronunciation: "KWA-troh", image: Four, translation: "Four", audioLetter: num4, audioWord: num4 },
+    { letter: "5", word: "cinco", letterPronunciation: "SEEN-koo", pronunciation: "SEEN-koo", image: Five, translation: "Five", audioLetter: num5, audioWord: num5 },
+    { letter: "6", word: "seis", letterPronunciation: "saysh", pronunciation: "SAYSH", image: Six, translation: "Six", audioLetter: num6, audioWord: num6 },
+    { letter: "7", word: "sete", letterPronunciation: "SEH-chee", pronunciation: "SEH-chee", image: Seven, translation: "Seven", audioLetter: num7, audioWord: num7 },
+    { letter: "8", word: "oito", letterPronunciation: "OY-too", pronunciation: "OY-too", image: Eight, translation: "Eight", audioLetter: num8, audioWord: num8 },
+    { letter: "9", word: "nove", letterPronunciation: "NOH-vee", pronunciation: "NOH-vee", image: Nine, translation: "Nine", audioLetter: num9, audioWord: num9 },
 ];
