8ef7abce94b9da06e384c9955a8742da3b5d05f5
yash56 <yash@connectex.io>
2025-11-17 10:19:11 -0700

    Persian Voice over done

--- data/persian.ts
+++ data/persian.ts
@@ -43,54 +43,137 @@ import Seven from "../assets/images/numbers/7.png";
 import Eight from "../assets/images/numbers/8.png";
 import Nine from "../assets/images/numbers/9.png";
 
+// LETTER AUDIO
+import letter1 from "../assets/sounds/persian/letter1.mp3";
+import letter2 from "../assets/sounds/persian/letter2.mp3";
+import letter3 from "../assets/sounds/persian/letter3.mp3";
+import letter4 from "../assets/sounds/persian/letter4.mp3";
+import letter5 from "../assets/sounds/persian/letter5.mp3";
+import letter6 from "../assets/sounds/persian/letter6.mp3";
+import letter7 from "../assets/sounds/persian/letter7.mp3";
+import letter8 from "../assets/sounds/persian/letter8.mp3";
+import letter9 from "../assets/sounds/persian/letter9.mp3";
+import letter10 from "../assets/sounds/persian/letter10.mp3";
+import letter11 from "../assets/sounds/persian/letter11.mp3";
+import letter12 from "../assets/sounds/persian/letter12.mp3";
+import letter13 from "../assets/sounds/persian/letter13.mp3";
+import letter14 from "../assets/sounds/persian/letter14.mp3";
+import letter15 from "../assets/sounds/persian/letter15.mp3";
+import letter16 from "../assets/sounds/persian/letter16.mp3";
+import letter17 from "../assets/sounds/persian/letter17.mp3";
+import letter18 from "../assets/sounds/persian/letter18.mp3";
+import letter19 from "../assets/sounds/persian/letter19.mp3";
+import letter20 from "../assets/sounds/persian/letter20.mp3";
+import letter21 from "../assets/sounds/persian/letter21.mp3";
+import letter22 from "../assets/sounds/persian/letter22.mp3";
+import letter23 from "../assets/sounds/persian/letter23.mp3";
+import letter24 from "../assets/sounds/persian/letter24.mp3";
+import letter25 from "../assets/sounds/persian/letter25.mp3";
+import letter26 from "../assets/sounds/persian/letter26.mp3";
+import letter27 from "../assets/sounds/persian/letter27.mp3";
+import letter28 from "../assets/sounds/persian/letter28.mp3";
+import letter29 from "../assets/sounds/persian/letter29.mp3";
+import letter30 from "../assets/sounds/persian/letter30.mp3";
+import letter31 from "../assets/sounds/persian/letter31.mp3";
+import letter32 from "../assets/sounds/persian/letter32.mp3";
+
+// WORD AUDIO
+import word1 from "../assets/sounds/persian/word1.mp3";
+import word2 from "../assets/sounds/persian/word2.mp3";
+import word3 from "../assets/sounds/persian/word3.mp3";
+import word4 from "../assets/sounds/persian/word4.mp3";
+import word5 from "../assets/sounds/persian/word5.mp3";
+import word6 from "../assets/sounds/persian/word6.mp3";
+import word7 from "../assets/sounds/persian/word7.mp3";
+import word8 from "../assets/sounds/persian/word8.mp3";
+import word9 from "../assets/sounds/persian/word9.mp3";
+import word10 from "../assets/sounds/persian/word10.mp3";
+import word11 from "../assets/sounds/persian/word11.mp3";
+import word12 from "../assets/sounds/persian/word12.mp3";
+import word13 from "../assets/sounds/persian/word13.mp3";
+import word14 from "../assets/sounds/persian/word14.mp3";
+import word15 from "../assets/sounds/persian/word15.mp3";
+import word16 from "../assets/sounds/persian/word16.mp3";
+import word17 from "../assets/sounds/persian/word17.mp3";
+import word18 from "../assets/sounds/persian/word18.mp3";
+import word19 from "../assets/sounds/persian/word19.mp3";
+import word20 from "../assets/sounds/persian/word20.mp3";
+import word21 from "../assets/sounds/persian/word21.mp3";
+import word22 from "../assets/sounds/persian/word22.mp3";
+import word23 from "../assets/sounds/persian/word23.mp3";
+import word24 from "../assets/sounds/persian/word24.mp3";
+import word25 from "../assets/sounds/persian/word25.mp3";
+import word26 from "../assets/sounds/persian/word26.mp3";
+import word27 from "../assets/sounds/persian/word27.mp3";
+import word28 from "../assets/sounds/persian/word28.mp3";
+import word29 from "../assets/sounds/persian/word29.mp3";
+import word30 from "../assets/sounds/persian/word30.mp3";
+import word31 from "../assets/sounds/persian/word31.mp3";
+import word32 from "../assets/sounds/persian/word32.mp3";
+
+// NUMBER AUDIO
+import num0 from "../assets/sounds/persian/zero.mp3";
+import num1 from "../assets/sounds/persian/one.mp3";
+import num2 from "../assets/sounds/persian/two.mp3";
+import num3 from "../assets/sounds/persian/three.mp3";
+import num4 from "../assets/sounds/persian/four.mp3";
+import num5 from "../assets/sounds/persian/five.mp3";
+import num6 from "../assets/sounds/persian/six.mp3";
+import num7 from "../assets/sounds/persian/seven.mp3";
+import num8 from "../assets/sounds/persian/eight.mp3";
+import num9 from "../assets/sounds/persian/nine.mp3";
+
+
 import { FlashCardType } from "../containers/flashCardTypes";
 
 // 🧠 Flashcard Array
 export const consonantCards: FlashCardType[] = [
-    { letter: 'ا', letterPronunciation: 'aa', pronunciation: 'Ab', word: 'آب', translation: 'Water', image: water },
-    { letter: 'ب', letterPronunciation: 'be', pronunciation: 'Baba', word: 'بابا', translation: 'Father', image: father },
-    { letter: 'پ', letterPronunciation: 'pe', pronunciation: 'Parande', word: 'پرنده', translation: 'Bird', image: bird },
-    { letter: 'ت', letterPronunciation: 'te', pronunciation: 'Tup', word: 'توپ', translation: 'Ball', image: ball },
-    { letter: 'ث', letterPronunciation: 'se', pronunciation: 'Servat', word: 'ثروت', translation: 'Wealth', image: wealth },
-    { letter: 'ج', letterPronunciation: 'je', pronunciation: 'Jangal', word: 'جنگل', translation: 'Forest', image: forest },
-    { letter: 'چ', letterPronunciation: 'che', pronunciation: 'Chay', word: 'چای', translation: 'Tea', image: tea },
-    { letter: 'ح', letterPronunciation: 'he', pronunciation: 'Hoz', word: 'حوض', translation: 'Pond', image: pond },
-    { letter: 'خ', letterPronunciation: 'khe', pronunciation: 'Khane', word: 'خانه', translation: 'House', image: house },
-    { letter: 'د', letterPronunciation: 'de', pronunciation: 'Dast', word: 'دست', translation: 'Hand', image: hand },
-    { letter: 'ذ', letterPronunciation: 'ze', pronunciation: 'Zorat', word: 'ذرت', translation: 'Corn', image: corn },
-    { letter: 'د', letterPronunciation: 'de', pronunciation: 'Dust', word: 'دوست', translation: 'Friend', image: friend },
-    { letter: 'ز', letterPronunciation: 'ze', pronunciation: 'Zarafe', word: 'زرافه', translation: 'Giraffe', image: giraffe },
-    { letter: 'ژ', letterPronunciation: 'zhe', pronunciation: 'Zhakat', word: 'ژاکت', translation: 'Jacket', image: jacket },
-    { letter: 'س', letterPronunciation: 'se', pronunciation: 'Sib', word: 'سیب', translation: 'Apple', image: apple },
-    { letter: 'ش', letterPronunciation: 'she', pronunciation: 'Shir', word: 'شیر', translation: 'Lion', image: lion },
-    { letter: 'ص', letterPronunciation: 'se', pronunciation: 'Sanduq', word: 'صندوق', translation: 'Box', image: box },
-    { letter: 'ض', letterPronunciation: 'ze', pronunciation: 'Zaif', word: 'ضعیف', translation: 'Weak', image: weak },
-    { letter: 'ط', letterPronunciation: 'te', pronunciation: 'Tanab', word: 'طناب', translation: 'Rope', image: rope },
-    { letter: 'ظ', letterPronunciation: 'ze', pronunciation: 'Zarf', word: 'ظرف', translation: 'Plate', image: plate },
-    { letter: 'ع', letterPronunciation: '‘ayn', pronunciation: 'Asal', word: 'عسل', translation: 'Honey', image: honey },
-    { letter: 'غ', letterPronunciation: 'ghe', pronunciation: 'Ghazâ', word: 'غذا', translation: 'Food', image: food },
-    { letter: 'ف', letterPronunciation: 'fe', pronunciation: 'Fil', word: 'فیل', translation: 'Elephant', image: elephant },
-    { letter: 'ق', letterPronunciation: 'ghaf', pronunciation: 'Ghalb', word: 'قلب', translation: 'Heart', image: heart },
-    { letter: 'ک', letterPronunciation: 'ke', pronunciation: 'Ketab', word: 'کتاب', translation: 'Book', image: book },
-    { letter: 'گ', letterPronunciation: 'ge', pronunciation: 'Gol', word: 'گل', translation: 'Flower', image: flower },
-    { letter: 'ل', letterPronunciation: 'le', pronunciation: 'Lab', word: 'لب', translation: 'Lip', image: lip },
-    { letter: 'م', letterPronunciation: 'me', pronunciation: 'Madar', word: 'مادر', translation: 'Mother', image: mother },
-    { letter: 'ن', letterPronunciation: 'ne', pronunciation: 'Nan', word: 'نان', translation: 'Bread', image: bread },
-    { letter: 'و', letterPronunciation: 'vâv', pronunciation: 'Varzesh', word: 'ورزش', translation: 'Exercise', image: exercise },
-    { letter: 'ه', letterPronunciation: 'he', pronunciation: 'Hedye', word: 'هدیه', translation: 'Gift', image: gift },
-    { letter: 'ی', letterPronunciation: 'ye', pronunciation: 'Yuzpalang', word: 'یوزپلنگ', translation: 'Cheetah', image: cheetah },
+    { letter: 'ا', letterPronunciation: 'aa', word: 'آب', pronunciation: 'Ab', translation: 'Water', image: water, audioLetter: letter1, audioWord: word1 },
+    { letter: 'ب', letterPronunciation: 'be', word: 'بابا', pronunciation: 'Baba', translation: 'Father', image: father, audioLetter: letter2, audioWord: word2 },
+    { letter: 'پ', letterPronunciation: 'pe', word: 'پرنده', pronunciation: 'Parande', translation: 'Bird', image: bird, audioLetter: letter3, audioWord: word3 },
+    { letter: 'ت', letterPronunciation: 'te', word: 'توپ', pronunciation: 'Tup', translation: 'Ball', image: ball, audioLetter: letter4, audioWord: word4 },
+    { letter: 'ث', letterPronunciation: 'se', word: 'ثروت', pronunciation: 'Servat', translation: 'Wealth', image: wealth, audioLetter: letter5, audioWord: word5 },
+    { letter: 'ج', letterPronunciation: 'je', word: 'جنگل', pronunciation: 'Jangal', translation: 'Forest', image: forest, audioLetter: letter6, audioWord: word6 },
+    { letter: 'چ', letterPronunciation: 'che', word: 'چای', pronunciation: 'Chay', translation: 'Tea', image: tea, audioLetter: letter7, audioWord: word7 },
+    { letter: 'ح', letterPronunciation: 'he', word: 'حوض', pronunciation: 'Hoz', translation: 'Pond', image: pond, audioLetter: letter8, audioWord: word8 },
+    { letter: 'خ', letterPronunciation: 'khe', word: 'خانه', pronunciation: 'Khane', translation: 'House', image: house, audioLetter: letter9, audioWord: word9 },
+    { letter: 'د', letterPronunciation: 'de', word: 'دست', pronunciation: 'Dast', translation: 'Hand', image: hand, audioLetter: letter10, audioWord: word10 },
+    { letter: 'ذ', letterPronunciation: 'ze', word: 'ذرت', pronunciation: 'Zorat', translation: 'Corn', image: corn, audioLetter: letter11, audioWord: word11 },
+    { letter: 'ر', letterPronunciation: 're', word: 'دوست', pronunciation: 'Dust', translation: 'Friend', image: friend, audioLetter: letter12, audioWord: word12 },
+    { letter: 'ز', letterPronunciation: 'ze', word: 'زرافه', pronunciation: 'Zarafe', translation: 'Giraffe', image: giraffe, audioLetter: letter13, audioWord: word13 },
+    { letter: 'ژ', letterPronunciation: 'zhe', word: 'ژاکت', pronunciation: 'Zhakat', translation: 'Jacket', image: jacket, audioLetter: letter14, audioWord: word14 },
+    { letter: 'س', letterPronunciation: 'se', word: 'سیب', pronunciation: 'Sib', translation: 'Apple', image: apple, audioLetter: letter15, audioWord: word15 },
+    { letter: 'ش', letterPronunciation: 'she', word: 'شیر', pronunciation: 'Shir', translation: 'Lion', image: lion, audioLetter: letter16, audioWord: word16 },
+    { letter: 'ص', letterPronunciation: 'se', word: 'صندوق', pronunciation: 'Sanduq', translation: 'Box', image: box, audioLetter: letter17, audioWord: word17 },
+    { letter: 'ض', letterPronunciation: 'ze', word: 'ضعیف', pronunciation: 'Zaif', translation: 'Weak', image: weak, audioLetter: letter18, audioWord: word18 },
+    { letter: 'ط', letterPronunciation: 'te', word: 'طناب', pronunciation: 'Tanab', translation: 'Rope', image: rope, audioLetter: letter19, audioWord: word19 },
+    { letter: 'ظ', letterPronunciation: 'ze', word: 'ظرف', pronunciation: 'Zarf', translation: 'Plate', image: plate, audioLetter: letter20, audioWord: word20 },
+    { letter: 'ع', letterPronunciation: '‘ayn', word: 'عسل', pronunciation: 'Asal', translation: 'Honey', image: honey, audioLetter: letter21, audioWord: word21 },
+    { letter: 'غ', letterPronunciation: 'ghe', word: 'غذا', pronunciation: 'Ghazâ', translation: 'Food', image: food, audioLetter: letter22, audioWord: word22 },
+    { letter: 'ف', letterPronunciation: 'fe', word: 'فیل', pronunciation: 'Fil', translation: 'Elephant', image: elephant, audioLetter: letter23, audioWord: word23 },
+    { letter: 'ق', letterPronunciation: 'ghaf', word: 'قلب', pronunciation: 'Ghalb', translation: 'Heart', image: heart, audioLetter: letter24, audioWord: word24 },
+    { letter: 'ک', letterPronunciation: 'ke', word: 'کتاب', pronunciation: 'Ketab', translation: 'Book', image: book, audioLetter: letter25, audioWord: word25 },
+    { letter: 'گ', letterPronunciation: 'ge', word: 'گل', pronunciation: 'Gol', translation: 'Flower', image: flower, audioLetter: letter26, audioWord: word26 },
+    { letter: 'ل', letterPronunciation: 'le', word: 'لب', pronunciation: 'Lab', translation: 'Lip', image: lip, audioLetter: letter27, audioWord: word27 },
+    { letter: 'م', letterPronunciation: 'me', word: 'مادر', pronunciation: 'Madar', translation: 'Mother', image: mother, audioLetter: letter28, audioWord: word28 },
+    { letter: 'ن', letterPronunciation: 'ne', word: 'نان', pronunciation: 'Nan', translation: 'Bread', image: bread, audioLetter: letter29, audioWord: word29 },
+    { letter: 'و', letterPronunciation: 'vâv', word: 'ورزش', pronunciation: 'Varzesh', translation: 'Exercise', image: exercise, audioLetter: letter30, audioWord: word30 },
+    { letter: 'ه', letterPronunciation: 'he', word: 'هدیه', pronunciation: 'Hedye', translation: 'Gift', image: gift, audioLetter: letter31, audioWord: word31 },
+    { letter: 'ی', letterPronunciation: 'ye', word: 'یوزپلنگ', pronunciation: 'Yuzpalang', translation: 'Cheetah', image: cheetah, audioLetter: letter32, audioWord: word32 },
 ];
+
 
 
 export const numberCards: FlashCardType[] = [
-    { letter: "۰", word: "صفر", letterPronunciation: "sefr", pronunciation: "SEFR", image: Zero, translation: "Zero" },
-    { letter: "۱", word: "یک", letterPronunciation: "yek", pronunciation: "YEK", image: One, translation: "One" },
-    { letter: "۲", word: "دو", letterPronunciation: "do", pronunciation: "DO", image: Two, translation: "Two" },
-    { letter: "۳", word: "سه", letterPronunciation: "seh", pronunciation: "SEH", image: Three, translation: "Three" },
-    { letter: "۴", word: "چهار", letterPronunciation: "chahar", pronunciation: "CHA-har", image: Four, translation: "Four" },
-    { letter: "۵", word: "پنج", letterPronunciation: "panj", pronunciation: "PANJ", image: Five, translation: "Five" },
-    { letter: "۶", word: "شش", letterPronunciation: "shesh", pronunciation: "SHESH", image: Six, translation: "Six" },
-    { letter: "۷", word: "هفت", letterPronunciation: "haft", pronunciation: "HAFT", image: Seven, translation: "Seven" },
-    { letter: "۸", word: "هشت", letterPronunciation: "hasht", pronunciation: "HASHT", image: Eight, translation: "Eight" },
-    { letter: "۹", word: "نه", letterPronunciation: "noh", pronunciation: "NOH", image: Nine, translation: "Nine" },
+    { letter: "۰", word: "صفر", letterPronunciation: "sefr", pronunciation: "SEFR", image: Zero, translation: "Zero", audioLetter: num0, audioWord: num0 },
+    { letter: "۱", word: "یک", letterPronunciation: "yek", pronunciation: "YEK", image: One, translation: "One", audioLetter: num1, audioWord: num1 },
+    { letter: "۲", word: "دو", letterPronunciation: "do", pronunciation: "DO", image: Two, translation: "Two", audioLetter: num2, audioWord: num2 },
+    { letter: "۳", word: "سه", letterPronunciation: "seh", pronunciation: "SEH", image: Three, translation: "Three", audioLetter: num3, audioWord: num3 },
+    { letter: "۴", word: "چهار", letterPronunciation: "chahar", pronunciation: "CHA-har", image: Four, translation: "Four", audioLetter: num4, audioWord: num4 },
+    { letter: "۵", word: "پنج", letterPronunciation: "panj", pronunciation: "PANJ", image: Five, translation: "Five", audioLetter: num5, audioWord: num5 },
+    { letter: "۶", word: "شش", letterPronunciation: "shesh", pronunciation: "SHESH", image: Six, translation: "Six", audioLetter: num6, audioWord: num6 },
+    { letter: "۷", word: "هفت", letterPronunciation: "haft", pronunciation: "HAFT", image: Seven, translation: "Seven", audioLetter: num7, audioWord: num7 },
+    { letter: "۸", word: "هشت", letterPronunciation: "hasht", pronunciation: "HASHT", image: Eight, translation: "Eight", audioLetter: num8, audioWord: num8 },
+    { letter: "۹", word: "نه", letterPronunciation: "noh", pronunciation: "NOH", image: Nine, translation: "Nine", audioLetter: num9, audioWord: num9 },
 ];
+
